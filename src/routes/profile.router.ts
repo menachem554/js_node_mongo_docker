d52db9c4574798b07ab448b697f92341ac803fc9
menachem554 <mendyrivkin@gmail.com>
2022-07-11 15:45:32 +0300

    m

--- src/routes/profile.router.ts
+++ src/routes/profile.router.ts
@@ -3,9 +3,10 @@ import * as profileController from '../controllers/profile.controllers';
 
 const router = express.Router();
 
-router.get('/', profileController.mainPage);
+router.get('/main', profileController.mainPage);
 router.get('/profile-picture', profileController.profilePicture);
+router.post('/create-profile', profileController.updateProfile);
 router.post('/update-profile', profileController.updateProfile);
-router.post('/get-profile', profileController.getProfile);
+router.get('/get-profile/', profileController.getProfile);
 
 export default router;
